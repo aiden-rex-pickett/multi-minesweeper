0302e4338661002802277eccab0860316fe9df80
Aiden Pickett <Ka7nqk@gmail.com>
2026-08-21 18:05:45 -0600

    Added file that will become the renderer interface

---
+++ src/renderer.zig
@@ -0,0 +1,6 @@
+//! TODO: This will eventually be an interface for rendering the minesweeper game. there will be likely a terminal and OpenGL implementation
+//!
+//! Possible methods:
+//!   - renderBoard
+//!   - renderBoardDebug (Shows mines)
+//!   - getInput (Gets a click, probably wont be used till after open cell is implemented)
