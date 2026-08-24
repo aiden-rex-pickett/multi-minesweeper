ecb2251761440399c2dea3fd47f72d707b3a68bf
Aiden Pickett <Ka7nqk@gmail.com>
2026-08-23 21:11:10 -0600

    Got the skeleton of the interface built out

--- src/renderer.zig
+++ src/renderer.zig
@@ -3,4 +3,36 @@
 //! Possible methods:
 //!   - renderBoard
 //!   - renderBoardDebug (Shows mines)
-//!   - getInput (Gets a click, probably wont be used till after open cell is implemented)
+//!   - getUserInput (Gets a click, probably wont be used till after open cell is implemented)
+
+const Self = @This();
+
+const GameBoard = @import("game_board.zig");
+
+ptr: *anyopaque,
+vtable: *const VTable,
+
+pub const VTable = struct {
+    renderBoard: *const fn (ctx: *anyopaque, board: *const GameBoard) anyerror!void,
+    renderBoardDebug: *const fn (ctx: *anyopaque, board: *const GameBoard) anyerror!void,
+    getUserInput: *const fn (ctx: *anyopaque) anyerror!Action,
+};
+
+// TODO: Maybe move to GameBoard struct? Might make more sense
+pub const Action = struct {
+    row: u32,
+    col: u32,
+    flag: bool,
+};
+
+pub fn renderBoard(self: Self, board: *const GameBoard) anyerror!void {
+    self.vtable.renderBoard(self.ptr, board);
+}
+
+pub fn renderBoardDebug(self: Self, board: *const GameBoard) anyerror!void {
+    self.vtable.renderBoardDebug(self.ptr, board);
+}
+
+pub fn getUserInput(self: Self) anyerror!Action {
+    return self.vtable.getUserInput(self.ptr);
+}
